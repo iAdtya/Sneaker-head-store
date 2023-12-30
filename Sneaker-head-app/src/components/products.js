0370908ffef5c4097d566d5f9249891ada1dc05a
Aditya Khedekar <adityakhedekar98906@gmail.com>
2023-12-30 21:03:58 +0530

    Update index.css and products.js

--- Sneaker-head-app/src/components/products.js
+++ Sneaker-head-app/src/components/products.js
@@ -3,22 +3,23 @@ import { productSelector } from "../redux/Reducers/ProductReducers";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fetchProducts } from "../redux/Reducers/ProductReducers";
-import { NavLink } from "react-router-dom";
-import {addToCart} from "../redux/Reducers/ProductReducers";
-
+import { addToCart } from "../redux/Reducers/ProductReducers";
 
 const Products = () => {
   const products = useSelector(productSelector);
   const dispatch = useDispatch();
 
-  dispatch(addToCart(products));
+  const handleAddToCart = (product) => {
+    dispatch(addToCart(product));
+  };
 
   useEffect(() => {
-    dispatch(fetchProducts());
+    dispatch(fetchProducts()).catch((error) => {
+      console.error("Failed to fetch products:", error);
+    });
   }, [dispatch]);
 
   useEffect(() => {
-    document.body.style.zoom = "75%";
     console.log(products);
   }, [products]);
 
@@ -26,45 +27,46 @@ const Products = () => {
     <>
       <div className="p-4">
         <h1 className=" text-2xl font-bold mb-2 mt-6 text-center inline-flex animate-text-gradient bg-gradient-to-r from-[#000000] via-[#7e02fa] to-[#ee1919] bg-[200%_auto] bg-clip-text  text-transparent">
-          Hii, Sneaker Head!!
+          Welcome, Sneaker Head!!
         </h1>
         <div className="flex justify-center ">
           <div className="grid grid-cols-4 gap-4 items-center  ">
-            {products.map((products) => (
+            {products.map((product) => (
               <div
                 className="card card-compact w-full bg-base-100 shadow-xl rounded-3xl"
-                key={products.id}
+                key={product.id}
               >
                 <figure>
                   <img
                     className="w-full h-full object-cover rounded-3xl"
-                    src={products.img}
-                    alt={products.name}
+                    src={product.img}
+                    alt={product.name}
                   />
                 </figure>
                 <div className="card-body">
                   <h2 className="card-title">
-                    Name:<span className="text-center">{products.name}</span>
+                    Name:<span className="text-center">{product.name}</span>
                   </h2>
 
                   <div className="h-44">
                     <p>
                       <span className="font-bold ">Description: </span>
-                      {products.description}
+                      {product.description}
                     </p>
                   </div>
 
                   <div className="flex justify-between">
                     <div className="mt-4">
-                      <span className="font-bold">MRP: ₹ {products.MRP}</span>
+                      <span className="font-bold">MRP: ₹ {product.MRP}</span>
                     </div>
+
                     <div className="card-actions justify-end">
-                      <NavLink
-                        to="/cart"
+                      <button
+                        onClick={() => handleAddToCart(product)}
                         className="btn btn-primary rounded-3xl"
                       >
                         Add to Cart
-                      </NavLink>
+                      </button>
                     </div>
                   </div>
                 </div>
